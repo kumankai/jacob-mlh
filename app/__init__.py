5167572a48531d3c378c0ce16c4a379646b95d1c
Jacob Angga <anggaj@sheridancollege.ca>
2024-06-28 17:10:46 -0600

    Added templates

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,31 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+work_experience = [
+    {"company": "Tim Hortons", "position": "Crew Member"},
+]
 
-@app.route('/')
+education = [
+    {
+        "institution": "Sheridan College",
+        "degree": "Honors Bachelors of Computer Science",
+        "date": "2022 - Present",
+    },
+]
+
+hobbies = [
+    {"hobby": "Basketball", "image": ""},
+    {"hobby": "Guitar", "image": ""},
+    {"hobby": "Eating", "image": ""},
+]
+
+
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template(
+        "index.html",
+        title="MLH Fellow",
+        url=os.getenv("URL"),
+        work_experience=work_experience,
+        education=education,
+    )
