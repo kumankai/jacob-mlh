7d0cf2cbe0cad4c161560557514d6f4c321654fe
Jacob Angga <anggaj@sheridancollege.ca>
2024-07-08 16:15:21 -0600

    Added a form for posts

--- app/__init__.py
+++ app/__init__.py
@@ -78,6 +78,11 @@ def hobbies():
     )
 
 
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html", title="Timeline", url=os.getenv("URL"))
+
+
 @app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
     name = request.form["name"]
