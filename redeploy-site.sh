9be5d22019896a4b32965050182d1deb3dab889e
Jacob Angga <anggaj@sheridancollege.ca>
2024-07-29 13:44:21 -0600

    Made locations font bold updated redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,4 +2,5 @@
 cd jacob-mlh
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
+docker rmi jacob-mlh-myportfolio:latest
 docker compose -f docker-compose.prod.yml up -d
